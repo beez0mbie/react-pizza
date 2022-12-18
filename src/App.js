9951ed2b7e34271719e13c2f0690ce24f89c4ce5
beez0mbie <vzhik3268@gmail.com>
2022-12-19 00:10:51 +0300

    add styles to header logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import './App.css';
+import './scss/app.scss';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <header className="header">
+          <div className="header__logo">
+            <img
+              className="header__logo-image"
+              src="img/logo_pizza.svg"
+              alt="Logo"
+            />
+            <div className="header__logo-descriprion">
+              <h1 className="header__logo-title">React pizza</h1>
+              <p className="header__logo-desc">
+                самая вкусная пицца во вселенной
+              </p>
+            </div>
+          </div>
+          <ul>
+            <li>1999 руб</li>
+            <li>Cart</li>
+          </ul>
+        </header>
+      </div>
+    </div>
+  );
 }
 
 export default App;
